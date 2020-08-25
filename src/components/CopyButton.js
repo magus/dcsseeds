07a01d1896486faca6f96e3772198dc5d611372e
magus <noah@iamnoah.com>
2020-08-25 01:25:25 -0700

    CopyButton: copied message state

--- src/components/CopyButton.js
+++ src/components/CopyButton.js
@@ -2,13 +2,36 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 export default function CopyButton({ children }) {
+  const instance = React.useRef({
+    ref: React.createRef(),
+    width: null,
+  });
+  const [copied, set_copied] = React.useState(false);
+
   function handleClick() {
     if (navigator.clipboard) {
       navigator.clipboard.writeText(children);
+
+      instance.current.width = instance.current.ref.current.offsetWidth;
+      set_copied(true);
     }
   }
 
-  return <Button onClick={handleClick}>{children}</Button>;
+  React.useEffect(() => {
+    if (copied) {
+      setTimeout(() => {
+        set_copied(false);
+      }, 2000);
+    }
+  }, [copied]);
+
+  const { width } = instance.current;
+
+  return (
+    <Button ref={instance.current.ref} onClick={handleClick} style={{ width }}>
+      {copied ? 'Copied!' : children}
+    </Button>
+  );
 }
 
 const Button = styled.button`
