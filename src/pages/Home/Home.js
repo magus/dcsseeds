1ec5a99c0887c8b00af680ce19b3d8af1afa521c
magus <noah@iamnoah.com>
2020-08-25 02:21:41 -0700

    Home: style recent seeds

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,25 +8,36 @@ export default function Home(props) {
   const { loading, error, data } = useQuery(RECENT_SEEDS_GQL);
 
   if (loading) {
-    return <Container>Loading...</Container>;
+    return (
+      <Container>
+        <LoadingText>Loading...</LoadingText>
+      </Container>
+    );
   }
 
   return (
     <Container>
-      {data.recentSeeds.map((seedRow) => {
-        return (
-          <div key={seedRow.id}>
-            <CopyButton>{seedRow.value}</CopyButton>
-          </div>
-        );
-      })}
+      <RecentSeeds>
+        {data.recentSeeds.map((seedRow) => {
+          return (
+            <SeedRow key={Math.random()}>
+              <Timestamp>{seedRow.created}</Timestamp>
+              <CopyButton>{seedRow.value}</CopyButton>
+            </SeedRow>
+          );
+        })}
+      </RecentSeeds>
     </Container>
   );
 }
 
+function Timestamp({ children: timestamp }) {
+  return <TimestampText>{new Date(timestamp).toLocaleString()}</TimestampText>;
+}
+
 const RECENT_SEEDS_GQL = gql`
-  query {
-    recentSeeds: seed(limit: 5) {
+  {
+    recentSeeds: seed(limit: 5, order_by: { created: desc }) {
       id
       value
       created
@@ -35,8 +46,32 @@ const RECENT_SEEDS_GQL = gql`
 `;
 
 const Container = styled.div`
-  height: 100%;
+  min-height: 100%;
+  overflow: scroll;
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const LoadingText = styled.div`
+  font-size: 48px;
+  font-weight: 800;
+`;
+
+const RecentSeeds = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SeedRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 24px 0;
+  > * {
+    margin: 8px 0;
+  }
+`;
+
+const TimestampText = styled.div`
+  font-size: 14px;
 `;
