a0e13d8d5937c2b2fc7faa5bf9295d213b04ccda
magus <noah@iamnoah.com>
2020-08-28 01:39:58 -0700

    server/utils/fetch: debug with fetch

--- src/server/utils/fetch.js
+++ src/server/utils/fetch.js
@@ -7,6 +7,6 @@ const DEFAULT_USER_AGENT = `${env.APP_NAME}-Bot/1.0`;
 module.exports = async (url, args = {}) => {
   args.headers = args.headers || {};
   args.headers['user-agent'] = DEFAULT_USER_AGENT;
-  console.error('url', url);
+  console.debug('[fetch]', url);
   return fetch(url, args);
 };
