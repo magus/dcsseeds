12216754c3a91e23ad919e81fb124801178d2f3a
magus <noah@iamnoah.com>
2020-08-30 04:36:28 -0700

    config/env: dcss.now.sh

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 const EnvConfig = {
   APP_NAME: 'dcsseeds',
   PROTOCOL: 'https',
-  HOSTNAME: 'dcsseeds.now.sh',
+  HOSTNAME: 'dcss.now.sh',
   SENTRY_DSN: 'https://c30a968049c246739657687af5d2fabb@o438648.ingest.sentry.io/5403737',
   GOOGLE_ANALYTICS_UA: 'UA-106090287-2',
 };
