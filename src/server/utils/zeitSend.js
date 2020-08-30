fbae2c53b8fa4fa418381d3dafe68a215cab44f5
magus <noah@iamnoah.com>
2020-08-30 04:39:50 -0700

    server/utils/zeitSend: err parsing and data format

--- src/server/utils/zeitSend.js
+++ src/server/utils/zeitSend.js
@@ -1,6 +1,23 @@
 const { send } = require('micro');
 
-module.exports = function zeitSend(res, statusCode, data) {
-  console.debug(JSON.stringify(data) || 'no data');
+module.exports = function zeitSend(res, statusCode, data, err) {
+  console.debug('[zeitSend]', statusCode, JSON.stringify({ data, err: err && err.message }));
+
+  if (!data || typeof data !== 'object') {
+    data = { __originalData: data };
+  }
+
+  if (!err) {
+    data.error = false;
+  } else {
+    data.error = true;
+
+    if (err.stack) {
+      data.stack = err.stack.split('\n');
+    } else {
+      data.rawError = err;
+    }
+  }
+
   return send(res, statusCode, data);
 };
