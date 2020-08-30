77d3f4310d2e3ddb1b408c4d395ec7b7b231becf
magus <noah@iamnoah.com>
2020-08-30 04:35:45 -0700

    config/env: include protocol and host

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,15 @@
-module.exports = {
+const EnvConfig = {
   APP_NAME: 'dcsseeds',
+  PROTOCOL: 'https',
   HOSTNAME: 'dcsseeds.now.sh',
   SENTRY_DSN: 'https://c30a968049c246739657687af5d2fabb@o438648.ingest.sentry.io/5403737',
   GOOGLE_ANALYTICS_UA: 'UA-106090287-2',
 };
+
+const dev = process.env.NODE_ENV !== 'production';
+if (dev) {
+  EnvConfig.PROTOCOL = 'http';
+  EnvConfig.HOSTNAME = 'localhost:3000';
+}
+
+module.exports = EnvConfig;
