219181d9607830f681212da5ba1c1412bf0d0b1e
magus <noah@iamnoah.com>
2020-09-20 16:17:30 -0700

    Compare: clean redesign and query formats

--- src/pages/Compare/Compare.js
+++ src/pages/Compare/Compare.js
@@ -30,24 +30,146 @@ export default function Compare(props) {
   }
 
   const gameCount = data.playerA.aggregate.count;
+  const playersOrder = [playerA, playerB];
+
+  const comparedSeeds = data.compareSeeds.map((seed) => {
+    const maxScore = Math.max(...seed.players.map((_) => _.score));
+    const players = [...seed.players];
+    players.sort((a, b) => (playersOrder.indexOf(a.name) > playersOrder.indexOf(b.name) ? 1 : -1));
+    const winners = seed.players.filter((_) => _.score === maxScore);
+    const winner = winners.length > 1 ? null : winners[0];
+
+    return { ...seed, players, maxScore, winner };
+  });
+
+  const winCounts = {};
+  comparedSeeds.forEach((seed) => {
+    if (!winCounts[seed.winner.name]) {
+      winCounts[seed.winner.name] = 0;
+    }
+
+    winCounts[seed.winner.name] += 1;
+  });
 
   return (
     <Container>
-      <div>{gameCount} Games</div>
-      <PlayerScore name={playerA} playerData={data.playerA} />
-      <PlayerScore name={playerB} playerData={data.playerB} />
+      <CompareTitle>{gameCount} Games</CompareTitle>
+
+      <CompareSeeds>
+        <Total>Total</Total>
+        {/* aggregate */}
+        <CompareSeedsRow>
+          <CompareSeedsPlayer isNormal isBold={false} color={playerColors[0]}>
+            <PlayerColumn>{playerA}</PlayerColumn>
+            <ScoreColumn>
+              <Score>{data.playerA.aggregate.sum.score}</Score>
+            </ScoreColumn>
+          </CompareSeedsPlayer>
+          <CompareSeedsPlayer isNormal isBold={false} color={playerColors[1]}>
+            <PlayerColumn>{playerB}</PlayerColumn>
+            <ScoreColumn>
+              <Score>{data.playerB.aggregate.sum.score}</Score>
+            </ScoreColumn>
+          </CompareSeedsPlayer>
+        </CompareSeedsRow>
+
+        <TotalDivider />
+
+        {/* for each seed */}
+        {comparedSeeds.map((seed) => {
+          return <CompareSeed key={seed.id} seed={seed} playersOrder={playersOrder} />;
+        })}
+      </CompareSeeds>
     </Container>
   );
 }
 
-function PlayerScore({ name, playerData }) {
-  const { score } = playerData.aggregate.sum;
+function CompareSeed({ seed }) {
+  const { players, maxScore } = seed;
+
   return (
-    <div>
-      {name}({score})
+    <CompareSeedsRow>
+      {players.map((player, i) => {
+        const isWinner = maxScore === player.score;
+
+        return (
+          <CompareSeedsPlayer key={player.name} color={isWinner ? playerColors[i] : loserColor}>
+            <PlayerColumn>{player.name}</PlayerColumn>
+            <ScoreColumn>
+              <Score>{player.score}</Score>
+            </ScoreColumn>
+          </CompareSeedsPlayer>
+        );
+      })}
+    </CompareSeedsRow>
+  );
+}
+
+const playerColors = ['#F6AD55', '#81E6D9'];
+const loserColor = 'rgba(255, 255, 255, 0.40)';
+
+const scoreFormatter = new Intl.NumberFormat('en');
+
+function Score(props) {
+  return scoreFormatter.format(props.children);
+}
+
+function TableLayoutRow({ data, widths }) {
+  return (
+    <div style={{ display: 'flex' }}>
+      {data.map((col, i) => (
+        <div style={{ flex: widths[i] || 0 }}>{col}</div>
+      ))}
     </div>
   );
 }
+
+const CompareSeeds = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 640px;
+  min-width: 320px;
+`;
+
+const CompareSeedsRow = styled.div`
+  margin: 8px 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CompareSeedsPlayer = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: ${(props) => props.color || 'inherit'};
+  font-weight: ${(props) => (props.isNormal ? 'inherit' : props.isBold ? 800 : 200)};
+`;
+
+const PlayerColumn = styled.div`
+  flex: 0.3;
+  font-weight: ${(props) => (props.isBold ? 800 : 200)};
+`;
+
+const ScoreColumn = styled.div`
+  flex: 0.7;
+  text-align: right;
+  font-variant: tabular-nums;
+`;
+
+const CompareTitle = styled.div`
+  font-size: 24px;
+  margin: 24px 0;
+`;
+
+const Total = styled.div`
+  font-size: 24px;
+`;
+
+const TotalDivider = styled.div`
+  margin: 0 0 12px 0;
+  background-color: white;
+  height: 1px;
+  width: 100%;
+`;
 
 const Container = styled.div`
   max-width: 640px;
--- src/pages/Compare/index.js
+++ src/pages/Compare/index.js
@@ -4,22 +4,28 @@ import Page from 'src/components/Page';
 
 const COMPARE_TOKEN = '..';
 
+function getPlayers(router) {
+  const { playerA, playerB } = router.query;
+
+  // console.warn({ playerA, playerB });
+
+  if (!!~playerA.indexOf(COMPARE_TOKEN)) {
+    const [_pA, _pB] = playerA.split(COMPARE_TOKEN);
+    return [_pA, _pB];
+  } else {
+    return [playerA, playerB];
+  }
+}
+
 export default function ComparePage(props) {
   const router = useRouter();
-  const { compareSyntax } = router.query;
+  const [playerA, playerB] = getPlayers(router);
 
   const title = 'dcsseeds compare';
-  let description;
+  let description = 'Compare seed games of players';
 
-  const forwardProps = { playerA: undefined, playerB: undefined };
-
-  if (compareSyntax) {
-    const [playerA, playerB] = compareSyntax.split(COMPARE_TOKEN);
-    if (playerA && playerB) {
-      forwardProps.playerA = playerA;
-      forwardProps.playerB = playerB;
-      description = `Compare seed games of ${playerA} vs ${playerB}`;
-    }
+  if (playerA && playerB) {
+    description = `Compare seed games of ${playerA} vs ${playerB}`;
   }
 
   const headTagProps = { title, description };
@@ -27,7 +33,7 @@ export default function ComparePage(props) {
 
   // console.warn({ headTagProps });
 
-  return <InternalComparePage {...props} {...forwardProps} />;
+  return <InternalComparePage {...props} {...{ playerA, playerB }} />;
 }
 
 // Defining getInitialProps will force SSR for metatags
