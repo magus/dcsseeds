976973bc6f87069158744432ff5ef8967c14bbd1
magus <noah@iamnoah.com>
2020-09-21 02:05:30 -0700

    zeitSend: JIT Error

--- src/server/utils/zeitSend.js
+++ src/server/utils/zeitSend.js
@@ -3,6 +3,11 @@ const { send } = require('micro');
 module.exports = function zeitSend(res, statusCode, data) {
   const isError = statusCode === 500 || data instanceof Error;
 
+  // JIT ERROR is created if error and data is missing
+  if (isError && !data) {
+    data = new Error('JIT ERROR');
+  }
+
   console.debug('[zeitSend]', statusCode, isError ? data.message : JSON.stringify({ data }));
 
   const responseJson = {};
