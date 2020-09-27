89664ba9317c75949681908f990533f080d6549f
magus <noah@iamnoah.com>
2020-09-27 01:30:11 -0700

    api/submit: submit notes

--- src/api/submit.js
+++ src/api/submit.js
@@ -49,11 +49,8 @@ module.exports = async (req, res) => {
       runeCount,
     };
 
-    // get seed notes, create + update if necessary
-    const autoSeedNotes = items.map((_) => `[${_.location}] ${_.name}`).join('\n');
-
     // mutate to create seed player
-    const result = await query({
+    const createSeedPlayerResult = await query({
       query: CREATE_SEED_PLAYER,
       endpoint: GRAPHQL_ENDPOINT,
       variables,
@@ -62,15 +59,28 @@ module.exports = async (req, res) => {
       },
     });
 
-    const [newSeedPlayer] = result.data.insert_seed_player.returning;
+    // mutate to create seed notes
+    const seedNote = await getSeedNote({ seed: value, version, items });
+    const createSeedNoteResult = await query({
+      query: CREATE_SEED_NOTES,
+      endpoint: GRAPHQL_ENDPOINT,
+      variables: { seed: value, version, value: seedNote },
+      headers: {
+        'x-hasura-admin-secret': HASURA_ADMIN_SECRET,
+      },
+    });
 
-    if (newSeedPlayer) {
-      return send(res, 200, newSeedPlayer);
+    const [resultSeedPlayer] = createSeedPlayerResult.data.insert_seed_player.returning;
+    const [resultSeedNote] = createSeedNoteResult.data.insert_seed_note.returning;
+
+    if (resultSeedPlayer && resultSeedNote) {
+      return send(res, 200, { resultSeedPlayer, resultSeedNote });
     }
 
     return send(res, 500, {
-      message: 'Unable to create seed player',
-      result,
+      message: 'Unable to submit morgue',
+      createSeedPlayerResult,
+      createSeedNoteResult,
     });
   } catch (err) {
     return send(res, 500, err);
@@ -119,6 +129,69 @@ const CREATE_SEED_PLAYER = `
       returning {
         id
       }
+    }
+  }
+`;
+
+// {
+//   "seed": "12134159514649495260",
+//   "version": "0.25",
+//   "value": "D8 Maggie: Wyrmbane\nD11 store: +10 Black Knight's horse barding {ponderous, rPois rN+}\nO2 store: +2 hat of the Bear Spirit {Spirit, +Rage rN+ MR++}\nO2 store: +1 kite shield \"Niugexis\" {rElec Dex+4}\nL6: +10 rapier of Invincibility (weapon) {holy, MP+9 Int+3 Stlth+, short}"
+// }
+const CREATE_SEED_NOTES = `
+  mutation InsertSeedNotes($seed: String!, $version: String!, $value: String!) {
+    insert_seed_note(
+      objects: { seed: $seed, version: $version, value: $value }
+      on_conflict: { constraint: seed_note_pkey, update_columns: [updated, value] }
+    ) {
+      returning {
+        id
+      }
+    }
+  }
+`;
+
+async function getSeedNote({ seed, version, items }) {
+  // get seed notes, create + update if necessary
+  const autoSeedNotes = items.map((_) => `[${_.location}] ${_.name}`);
+  const existingSeedNotesQuery = await query({
+    query: GET_SEED_NOTES,
+    endpoint: GRAPHQL_ENDPOINT,
+    variables: { seed, version },
+    headers: {
+      'x-hasura-admin-secret': HASURA_ADMIN_SECRET,
+    },
+  });
+  // break apart by new lines
+  const [existingSeedNote] = existingSeedNotesQuery.data.seed_note;
+  const existingSeedNoteLines = existingSeedNote.value.split('\n');
+
+  // combine by looking at each row in both notes
+  // keep unique entries to ensure no overwriting
+  const combinedNotes = [];
+  const combinedNoteLookup = {};
+  function parseNoteLine(line) {
+    if (!combinedNoteLookup[line]) {
+      combinedNoteLookup[line] = true;
+      combinedNotes.push(line);
+    }
+  }
+  // parse lines of both existing seed notes and autoSeedNotes
+  existingSeedNoteLines.forEach(parseNoteLine);
+  autoSeedNotes.forEach(parseNoteLine);
+
+  return combinedNotes.join('\n');
+}
+
+// {
+//   "seed": "12134159514649495260",
+//   "version": "0.25"
+// }
+const GET_SEED_NOTES = `
+  query GetSeedNotes($seed: String!, $version: String!) {
+    seed_note(where: {version: {_eq: $version}, seed: {_eq: $seed}}) {
+      id
+      value
     }
   }
 `;
