01c2c79ea773a96613746096c6a69825e68c8aa3
magus <noah@iamnoah.com>
2021-03-23 05:01:16 -0700

    utils/random: client side random

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,11 +1,4 @@
-const crypto = require('crypto');
-
-const MAX_INT8 = Math.pow(2, 8) - 1;
-
-const random = () => {
-  const [randInt8] = crypto.randomBytes(1).toJSON().data;
-  return randInt8 / MAX_INT8;
-};
+const random = () => Math.random();
 
 const randomInt = (max = 9, min = 0) => Math.floor(random() * (max - min)) + min;
 
