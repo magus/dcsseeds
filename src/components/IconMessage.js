56dbf1ca4a514d52f9765ed5be3c9961680fe388
magus <noah@iamnoah.com>
2021-03-22 23:09:02 -0700

    IconMessage: generic icon with message

---
+++ src/components/IconMessage.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export function IconMessage(props) {
+  return (
+    <Container>
+      <Icon>{props.icon}</Icon>
+      <Message>{props.message}</Message>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Icon = styled.div`
+  font-size: 48px;
+`;
+
+const Message = styled.div`
+  font-size: var(--font-large);
+  font-weight: var(--font-heavy);
+`;
