d0d3dc0516a67b726fea7fabdec3cd0fb3c7a02b
magus <noah@iamnoah.com>
2021-03-24 15:50:31 -0700

    zeitSend: log entire error

--- src/server/utils/zeitSend.js
+++ src/server/utils/zeitSend.js
@@ -8,7 +8,7 @@ module.exports = function zeitSend(res, statusCode, data, { prettyPrint } = {}) 
     data = new Error('JIT ERROR');
   }
 
-  console.debug('[zeitSend]', statusCode, isError ? data.message : JSON.stringify({ data }));
+  console.debug('[zeitSend]', statusCode, isError ? data : JSON.stringify({ data }));
 
   const responseJson = {};
 
