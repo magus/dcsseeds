6da5ed9132924d8e81b0052abcd336b055952939
magus <noah@iamnoah.com>
2021-04-13 01:32:03 -0700

    dcss.now.sh redirects to dcss.vercel.app

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ const EnvConfig = {
   DEV: process.env.NODE_ENV !== 'production',
   APP_NAME: 'dcsseeds',
   PROTOCOL: 'https',
-  HOSTNAME: 'dcss.now.sh',
+  HOSTNAME: 'dcss.vercel.app',
   SENTRY_DSN: 'https://c30a968049c246739657687af5d2fabb@o438648.ingest.sentry.io/5403737',
   GOOGLE_ANALYTICS_UA: 'UA-106090287-2',
 };
