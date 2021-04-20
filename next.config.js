dee25a1757d031c9f737d145312fae104e98e7bc
magus <noah@iamnoah.com>
2021-04-19 17:11:52 -0700

    Security Headers
    CSP, nonce, x-frame-options, referrer, permissions-policy,
    Access-Control-Allow-Origin, etc.

--- next.config.js
+++ next.config.js
@@ -19,6 +19,20 @@ module.exports = withSourceMaps({
     SENTRY_TOKEN_HEADER,
   },
 
+  // https://securityheaders.com
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: securityHeaders,
+      },
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+
   webpack: (config, { isServer, buildId }) => {
     config.plugins.push(
       new webpack.DefinePlugin({
@@ -34,3 +48,40 @@ module.exports = withSourceMaps({
     return config;
   },
 });
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+  {
+    key: 'Access-Control-Allow-Origin',
+    value: `${EnvConfig.PROTOCOL}://${EnvConfig.HOSTNAME}`,
+  },
+];
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import { randomBytes } from 'crypto';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -16,8 +16,12 @@ export default class MyDocument extends Document {
         });
 
       const initialProps = await Document.getInitialProps(ctx);
+      const nonce = randomBytes(8).toString('base64');
+
       return {
         ...initialProps,
+        // nonce for csp
+        nonce,
         // Styles fragment is rendered after the app and page rendering finish
         styles: (
           <>
@@ -35,22 +39,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/*
-          https://developers.google.com/analytics/devguides/collection/gtagjs/events
-          https://developers.google.com/analytics/devguides/collection/gtagjs/sending-data
-          */}
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_UA}`}></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${process.env.GOOGLE_ANALYTICS_UA}');
-          `,
-            }}
-          />
+          <meta httpEquiv="Content-Security-Policy" content={getCsp(this.props.nonce)} />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           {/*
 
@@ -68,6 +57,24 @@ export default class MyDocument extends Document {
           {/* iOS */}
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
+          {/*
+          https://developers.google.com/analytics/devguides/collection/gtagjs/events
+          https://developers.google.com/analytics/devguides/collection/gtagjs/sending-data
+          */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_UA}`}></script>
+          <script
+            nonce={this.props.nonce}
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_UA}');
+          `,
+            }}
+          />
+
           {this.props.styleTags}
         </Head>
         <body>
@@ -78,3 +85,38 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+function getCsp(nonce) {
+  const policies = {
+    'script-src': ["'self'"],
+  };
+
+  // Include nonce for unsafe-inline scripts (e.g. google analytics)
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src#unsafe_inline_script
+  if (nonce) {
+    policies['script-src'].push(`'nonce-${nonce}'`);
+  }
+
+  // In dev we allow 'unsafe-eval', so HMR doesn't trigger the CSP
+  if (process.env.NODE_ENV !== 'production') {
+    policies['script-src'].push("'unsafe-eval'");
+  }
+  // Google Analytics
+  policies['script-src'].push('www.googletagmanager.com', 'www.google-analytics.com');
+
+  const ContentSecurityPolicy = `
+    default-src 'self';
+    ${getCSPString('script-src', policies)}
+    child-src 'none';
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self';
+  `;
+
+  return ContentSecurityPolicy.replace(/\n/g, '');
+}
+
+const getCSPString = (name, policies) => `${name} ${policies[name].join(' ')};`;
