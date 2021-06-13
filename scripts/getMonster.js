1341b2e6921444b8e2045b20f097537679762ded
magus <noah@iamnoah.com>
2021-06-13 03:59:27 -0700

    getMonster: parse tiles for local and remote paths

--- scripts/getMonster.js
+++ scripts/getMonster.js
@@ -4,6 +4,7 @@ const fs = require('fs').promises;
 const { CPPCompiler } = require('./cpp-parse/CPPCompiler');
 
 const VERSION = '0.26.1';
+const GITHUB_RAW = `https://raw.githubusercontent.com/crawl/crawl/${VERSION}`;
 
 // cd projRoot/crawl
 // git checkout <version>
@@ -38,8 +39,8 @@ const MONSTERENTRY_FIELDNAMES = [
   'itemUseType',
   'size',
   'shape',
-  'tile',
-  'tileCorpse',
+  'tileId',
+  'tileCorpseId',
 ];
 
 const MONSTERENTRY = Object.freeze(
@@ -50,9 +51,35 @@ const MONSTERENTRY = Object.freeze(
 );
 
 (async function run() {
+  const monsters = await getMonsterData();
+  const monTileMap = await getMonTileMap();
+
+  const monstersWithTiles = [];
+  monsters.forEach((monster) => {
+    let tilePath = monTileMap.get(monster.tileId);
+
+    const monsterWithTile = {
+      ...monster,
+      localTilePath: `./crawl/${tilePath}`,
+      githubTilePath: `${GITHUB_RAW}/${tilePath}`,
+    };
+
+    if (!monsterWithTile.localTilePath) {
+      console.error({ monster });
+      throw new Error(`[${monster.id}] missing localTilePath`);
+    }
+
+    monstersWithTiles.push(monsterWithTile);
+  });
+
+  // monstersWithTiles.forEach((monster) => console.debug(JSON.stringify(monster, null, 2)));
+  console.debug({ monstersWithTiles });
+})();
+
+async function getMonsterData() {
   const monsters = [];
 
-  const monData = await getMonData();
+  const monData = await parseFile('./crawl/crawl-ref/source/mon-data.h');
   monData.traverse({
     Assignment: {
       enter(node, parent) {
@@ -64,7 +91,7 @@ const MONSTERENTRY = Object.freeze(
             const entry = {};
             monsters.push(entry);
 
-            // each field of this spellbook template object is a spell name
+            // parse each field of this `monsterentry` struct
             monsterentry.fields.forEach((monsterentryField, i) => {
               const fieldName = MONSTERENTRY_FIELDNAMES[i];
 
@@ -84,7 +111,7 @@ const MONSTERENTRY = Object.freeze(
                 case MONSTERENTRY.itemUseType:
                 case MONSTERENTRY.size:
                 case MONSTERENTRY.shape:
-                case MONSTERENTRY.tileCorpse:
+                case MONSTERENTRY.tileCorpseId:
                   return (entry[fieldName] = expression('identifier', monsterentryField));
                 case MONSTERENTRY.flags:
                 case MONSTERENTRY.resists:
@@ -103,7 +130,7 @@ const MONSTERENTRY = Object.freeze(
                   return (entry[fieldName] = expression('boolean', monsterentryField));
                 case MONSTERENTRY.energyUsage:
                   return (entry[fieldName] = expression('energyUsage', monsterentryField));
-                case MONSTERENTRY.tile:
+                case MONSTERENTRY.tileId:
                   return (entry[fieldName] = expression('tile', monsterentryField));
 
                 default:
@@ -115,17 +142,23 @@ const MONSTERENTRY = Object.freeze(
                   );
               }
             });
+
+            //
+            // fixup entry
+            //
+
+            // correct tile id
+            entry.tileId = re(entry.tileId, RE.tileId);
           });
         }
       },
     },
   });
 
-  console.debug({ monsters });
-  debugger;
-})();
+  return monsters;
+}
 
-const DEFAULT_ENERGY_USAGE = {};
+const DEFAULT_ENERGY_USAGE = [10, 10, 10, 10, 10, 10, 10, 100];
 
 function expression(type, token) {
   switch (type) {
@@ -163,10 +196,53 @@ function expression(type, token) {
   }
 }
 
-async function getMonData() {
-  // TODO requires updates to lexer and parser to handle more tokens
-  const monData = await parseFile('./crawl/crawl-ref/source/mon-data.h');
-  return monData;
+async function getMonTileMap() {
+  const TILE_DIR = 'crawl-ref/source/rltiles';
+  const tileMap = new Map();
+  let currentDir = null;
+  const dcMonContent = await readFile(`./crawl/${TILE_DIR}/dc-mon.txt`);
+  const dcMonContentLines = dcMonContent.split('\n');
+
+  // first pass replace includes
+  const includedLines = [];
+  for (let i = 0; i < dcMonContentLines.length; i++) {
+    const line = dcMonContentLines[i];
+    const include = re(line, RE.include);
+    if (include) {
+      const includeContent = await readFile(`./crawl/${TILE_DIR}/${include}`);
+      includedLines.push(...includeContent.split('\n'));
+    } else {
+      includedLines.push(line);
+    }
+  }
+
+  includedLines.forEach((line) => {
+    // handle directory lines
+    // directory lines specify we are starting a new icon tile mapping
+    // e.g. %sdir mon/nonliving
+    let lineDir = re(line, RE.tileDir);
+    let outline = re(line, RE.outline);
+    let comment = re(line, RE.comment);
+    if (comment) {
+      // ignore # comment lines
+    } else if (outline) {
+      // ignore %rim outline lines
+    } else if (lineDir) {
+      currentDir = lineDir;
+    } else {
+      // handle tile mapping lines
+      // tile mapping lines specify the icon filename and the tile id
+      // e.g. tiamat_black MONS_TIAMAT
+      if (line) {
+        const [filename, tileId] = line.split(' ');
+        const tilePath = `${TILE_DIR}/${currentDir}/${filename}.png`;
+        tileMap.set(tileId, tilePath);
+        console.debug({ tileId, tilePath });
+      }
+    }
+  });
+
+  return tileMap;
 }
 
 async function parseFile(filename) {
@@ -178,3 +254,20 @@ async function readFile(filename) {
   let buffer = await fs.readFile(filename, { encoding: 'utf8', flag: 'r' });
   return buffer.toString();
 }
+
+function re(string, regex) {
+  const match = string.match(regex);
+  if (match) {
+    let [, firstGroup] = match;
+    return firstGroup;
+  }
+  return null;
+}
+
+const RE = {
+  tileId: /^TILEP_(.*)$/,
+  tileDir: /^%sdir (.*)$/,
+  outline: /^%rim (.*)$/,
+  comment: /^#(.*)$/,
+  include: /^%include (.*)$/,
+};
