e3787a4c9346ecfad587c1082eee954ab19384e3
magus <noah@iamnoah.com>
2022-02-06 15:37:23 -0800

    runRegex: simplify remove async

--- src/utils/runRegex.js
+++ src/utils/runRegex.js
@@ -1,22 +1,30 @@
-module.exports = async function regexAsync(id, content, regex) {
-  return new Promise((resolve, _reject) => {
-    const reject = (field) => {
-      const message = ['runRegex', id, field].join(' ');
-      // _reject(message);
-      throw new Error(message);
-    };
+module.exports = runRegex;
 
-    const match = content.match(regex);
-    if (!match) {
-      // console.error({ content, regex, match });
-      return reject(`match[${JSON.stringify(match)}]`);
-    }
+// throw error if no match to enforce match return type (no null)
+// guarantees that if this succeeds we have a full match and can parse groups
+function runRegex(id, content, regex) {
+  const match = runRegexSafe(id, content, regex);
 
+  // return match if present and at least one matching group
+  if (match) {
+    return match;
+  }
+
+  throw new Error(['runRegex', id, regex].join(' '));
+}
+
+// runRegex.safe can return null instead of throwing an error
+runRegex.safe = runRegexSafe;
+function runRegexSafe(id, content, regex) {
+  const match = content.match(regex);
+
+  // return match if present and at least one matching group
+  if (match) {
     const [, firstGroup] = match;
-    if (!firstGroup) {
-      return reject(`group[${JSON.stringify(match)}]`);
+    if (firstGroup) {
+      return match;
     }
+  }
 
-    return resolve(match);
-  });
-};
+  return null;
+}
