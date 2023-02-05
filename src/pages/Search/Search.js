bd76f232d703bc2a3b977887dec50e5d984913c8
magus <noah@iamnoah.com>
2023-02-04 16:22:10 -0800

    search: refactor ArtifactSearch component

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,105 +1,44 @@
 import * as React from 'react';
 import styled, { css } from 'styled-components';
 import { useRouter } from 'next/router';
-import { motion, AnimatePresence } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 
 import { useArtifactFilter } from 'src/graphql/useArtifactFilter';
-import { UNRANDS } from 'src/utils/Unrands';
 
 import { ItemSearch } from './components/ItemSearch';
+import { ArtifactSearch } from './components/ArtifactSearch';
 import * as Spacer from '../../components/Spacer';
 
-function ArtifactFilter(props) {
-  return (
-    <AnimatePresence>
-      <ArtifactFilterContainer>
-        {UNRANDS.map((name, i) => {
-          const active = props.filter_set.has(i);
-          const count = props.artifact_count[i];
-
-          function handle_click() {
-            // console.debug({ name, i });
-            if (count === 0) return;
-
-            if (active) {
-              props.remove_filter(i);
-            } else {
-              props.add_filter(i);
-            }
-          }
-
-          if (count === 0) {
-            return null;
-          }
-
-          return (
-            <ArtifactFilterButtonGroup
-              key={name}
-              // force line break
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              layout
-            >
-              <ArtifactFilterButton active={active} count={count} onClick={handle_click}>
-                {name} ({count})
-              </ArtifactFilterButton>
-            </ArtifactFilterButtonGroup>
-          );
-        })}
-      </ArtifactFilterContainer>
-    </AnimatePresence>
-  );
-}
-
-const ArtifactFilterContainer = styled.div`
-  flex-direction: row;
-`;
-
-const ArtifactFilterButtonGroup = styled(motion.div)`
-  display: inline-flex;
-  margin: 0 var(--spacer-2) var(--spacer-2) 0;
-`;
-
-const ArtifactFilterButton = styled.button`
-  font-size: var(--font-small);
-  padding: var(--spacer-d2) var(--spacer-1);
-  height: auto;
-  transition: color, background-color 0.2s ease-out;
-
-  ${(props) => {
-    switch (true) {
-      case props.active:
-        return css`
-          background-color: rgb(21, 128, 61);
-          color: rgb(220, 252, 231);
-        `;
-      case props.count === 0:
-        return css`
-          background-color: #171717;
-          color: #404040;
-        `;
-      default:
-        return '';
-    }
-  }}
-`;
-
-export default function Search(props) {
+function SearchInternal(props) {
   const artifact_filter = useArtifactFilter(props);
   console.debug('[artifact_filter]', artifact_filter);
 
   const router = useRouter();
 
-  return (
-    <Container>
-      <Spacer.Vertical size="2" />
+  if (artifact_filter.result_list.length) {
+    return <ArtifactSearch key="ArtifactSearch" {...artifact_filter} />;
+  } else if (router.query.q) {
+    return <ItemSearch {...props} />;
+  }
 
+  return (
+    <React.Fragment>
       <ItemSearch {...props} />
 
       <Spacer.Vertical size="2" />
 
-      <ArtifactFilter {...artifact_filter} />
+      <ArtifactSearch key="ArtifactSearch" {...artifact_filter} />
+    </React.Fragment>
+  );
+}
+
+export default function Search(props) {
+  return (
+    <Container>
+      <Spacer.Vertical size="2" />
+      <AnimatePresence>
+        <SearchInternal {...props} />
+      </AnimatePresence>
     </Container>
   );
 }
---
+++ src/pages/Search/components/ArtifactSearch.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import styled, { css } from 'styled-components';
+import { useRouter } from 'next/router';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import { useArtifactFilter } from 'src/graphql/useArtifactFilter';
+import { UNRANDS } from 'src/utils/Unrands';
+
+import * as Spacer from '../../../components/Spacer';
+
+export function ArtifactSearch(props) {
+  return (
+    <Container>
+      {UNRANDS.map((name, i) => {
+        const active = props.filter_set.has(i);
+        const count = props.artifact_count[i];
+
+        function handle_click() {
+          // console.debug({ name, i });
+          if (count === 0) return;
+
+          if (active) {
+            props.remove_filter(i);
+          } else {
+            props.add_filter(i);
+          }
+        }
+
+        if (count === 0) {
+          return null;
+        }
+
+        return (
+          <ButtonGroup
+            key={name}
+            // force line break
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+          >
+            <Button active={active} count={count} onClick={handle_click}>
+              {name} ({count})
+            </Button>
+          </ButtonGroup>
+        );
+      })}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  flex-direction: row;
+`;
+
+const ButtonGroup = styled(motion.div)`
+  display: inline-flex;
+  margin: 0 var(--spacer-2) var(--spacer-2) 0;
+`;
+
+const Button = styled.button`
+  font-size: var(--font-small);
+  padding: var(--spacer-d2) var(--spacer-1);
+  height: auto;
+  transition: color, background-color 0.2s ease-out;
+
+  ${(props) => {
+    switch (true) {
+      case props.active:
+        return css`
+          background-color: rgb(21, 128, 61);
+          color: rgb(220, 252, 231);
+        `;
+      case props.count === 0:
+        return css`
+          background-color: #171717;
+          color: #404040;
+        `;
+      default:
+        return '';
+    }
+  }}
+`;
