8e7aff59a722a40f08a4951394bf437e47e140f7
magus <noah@iamnoah.com>
2023-02-11 17:00:59 -0800

    chore: unused css import

--- src/pages/Search/components/ItemSearch.js
+++ src/pages/Search/components/ItemSearch.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import * as ScrapePlayers from 'src/graphql/scrapePlayers';
 
