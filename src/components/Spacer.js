9fbc4dcd4d3e3b141bf1d1239cd644c25eee6c7c
magus <noah@iamnoah.com>
2023-03-07 01:17:18 -0800

    Spacer: inline block and min dimensions

--- src/components/Spacer.js
+++ src/components/Spacer.js
@@ -3,16 +3,18 @@ import styled from 'styled-components';
 const SpacerSize = (props) => `var(--spacer-${props.size || 1})`;
 
 export const Square = styled.div`
-  width: ${SpacerSize};
-  height: ${SpacerSize};
+  display: inline-block;
+  min-width: ${SpacerSize};
+  min-height: ${SpacerSize};
 `;
 
 export const Horizontal = styled.div`
-  width: ${SpacerSize};
-  height: 100%;
+  min-width: ${SpacerSize};
+  min-height: 1;
 `;
 
 export const Vertical = styled.div`
-  width: 100%;
-  height: ${SpacerSize};
+  display: inline-block;
+  min-width: 1;
+  min-height: ${SpacerSize};
 `;
