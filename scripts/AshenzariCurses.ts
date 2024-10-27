3bc95185a79b8dbbc32f70cae10e29ed70ac3c08
magus <noah@iamnoah.com>
2024-10-26 20:20:10 -0700

    scripts/AshenzariCurses: refactor and track versions

--- scripts/AshenzariCurses.ts
+++ scripts/AshenzariCurses.ts
@@ -2,6 +2,8 @@
 import fs from 'fs';
 import path from 'path';
 import { execSync } from 'child_process';
+import isEqual from 'lodash/isEqual';
+import cloneDeep from 'lodash/cloneDeep';
 
 import { CPPCompiler } from 'scripts/cpp-parse/CPPCompiler';
 
@@ -15,30 +17,47 @@ import * as crawl_dir from './crawl_dir';
     parse_curse_list('0.28.0'),
     parse_curse_list('0.29.1'),
     parse_curse_list('0.30.0'),
+    parse_curse_list('0.31.0'),
+    parse_curse_list('0.32.1'),
   ]);
 
-  // ensure all version outputs are the same
-  // if not throws an error for manual investigation
-  let [current_result] = curse_result_list;
-  for (const curse_result of curse_result_list.slice(1)) {
-    if (!current_result) {
-      current_result = curse_result;
-    } else {
-      // compare
-      // throw new Error(`[${curse_result.version}] introduced change, requires manual investigation`);
-      for (let i = 0; i < current_result.list.length; i++) {
-        const before = current_result.list[i];
-        const after = curse_result.list[i];
+  // gather all curses by id
+  const curse_by_id: Record<string, Curse> = {};
 
-        if (JSON.stringify(before) !== JSON.stringify(after)) {
-          console.error({ before, after });
-          throw new Error(`[${curse_result.version}] introduced change requires manual investigation`);
-        }
+  for (const curse_result of curse_result_list) {
+    const version = curse_result.version;
+
+    for (const curse of curse_result.list) {
+      console.debug({ curse });
+
+      const maybe_curse = curse_by_id[curse.id];
+
+      if (!maybe_curse) {
+        const last_version = version;
+        curse_by_id[curse.id] = { ...curse, version, last_version };
+        continue;
       }
+
+      // compare maybe_curse and curse but ignore version fields
+      const compare_curse = cloneDeep(maybe_curse);
+      delete compare_curse.version;
+      delete compare_curse.last_version;
+
+      // ensure curse entry matches, update version
+      if (isEqual(compare_curse, curse)) {
+        maybe_curse.last_version = version;
+        continue;
+      }
+
+      console.error({ compare_curse, curse });
+      console.error();
+      throw new Error('curse-id-mismatch');
     }
   }
 
-  const curse_list = current_result.list;
+  console.debug({ curse_by_id });
+
+  const curse_list = Object.values(curse_by_id);
 
   // output
   const output_lines = ['// GENERATED'];
@@ -46,7 +65,7 @@ import * as crawl_dir from './crawl_dir';
   output_lines.push('');
   output_lines.push(`export const List = ${JSON.stringify(curse_list)};`);
 
-  const abbr_map: { [key: string]: Curse } = {};
+  const abbr_map: Record<string, Curse> = {};
   for (const curse of curse_list) {
     abbr_map[curse.abbr] = curse;
   }
@@ -57,6 +76,9 @@ import * as crawl_dir from './crawl_dir';
   const output_path = path.join(__dirname, '__output__', 'AshenzariCurses.ts');
   fs.writeFileSync(output_path, output_lines.join('\n'));
   execSync(`yarn prettier --write "${output_path}"`);
+
+  console.info('📋 Generated `AshenzariCurses.ts`');
+  console.info(output_path);
 
   crawl_dir.reset();
 })();
@@ -88,7 +110,13 @@ async function parse_curse_list(version: string): Promise<{ list: Array<Curse>; 
   return { list, version };
 }
 
-type Curse = { id: string; name: string; abbr: string };
+type Curse = {
+  id: string;
+  name: string;
+  abbr: string;
+  version?: string;
+  last_version?: string;
+};
 
 function extract_curse(object: any): Curse {
   const curse = {
