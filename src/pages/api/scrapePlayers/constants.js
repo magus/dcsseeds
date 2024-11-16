a4e8e12b0b9ad09c42860904c9e5ee2019578440
magus <noah@iamnoah.com>
2024-11-16 14:52:37 -0800

    scrapePlayers: check for newer versions

--- src/pages/api/scrapePlayers/constants.js
+++ src/pages/api/scrapePlayers/constants.js
@@ -5,6 +5,6 @@ import { make_version_gate } from './make_version_gate';
 // e.g. https://github.com/crawl/crawl/tree/0.27.1
 export const MINIMUM_ALLOWED_DATE = new Date('2021-08-18');
 
-export const VERSION_LIST = ['0.27', '0.28', '0.29'];
+export const VERSION_LIST = ['0.27', '0.28', '0.29', '0.30', '0.31', '0.32'];
 
 export const version_gate = make_version_gate(['0.27.1', '0.28.0', '0.29.1']);
